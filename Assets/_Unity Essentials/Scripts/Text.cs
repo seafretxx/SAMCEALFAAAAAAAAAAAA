483667ae1f4bbd28a7f4aa3e19bcb96e4b249d53
MikolajSliwa <moki.sliwa@op.pl>
2026-01-21 16:20:50 +0100

    Triggery fajne

---
+++ Assets/_Unity Essentials/Scripts/Text.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(false);
+        }
+    }
+}
