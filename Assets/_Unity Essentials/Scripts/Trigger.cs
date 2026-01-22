a68453704bca350d1c663935958e8f2a25385e03
kali <147497988+kalimrooz@users.noreply.github.com>
2026-01-22 09:16:45 +0100

    Triggery

---
+++ Assets/_Unity Essentials/Scripts/Trigger.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(false);
+        }
+    }
+}
